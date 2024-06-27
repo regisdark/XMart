8af3bdf05d037abf6bd59958153021cc6e9e8ff6
Eugenio Avila <avilamqz@hotmail.com>
2024-06-27 01:06:49 -0600

    Proyecto: Apis

---
+++ BusinessLogic/Metodos/Apis.cs
@@ -0,0 +1,30 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Metodos
+{
+    public class Apis : ManejadorModelo<VENTA>
+    {
+        public IQueryable<object> ObtenerVentasSimple()
+        {
+			try
+			{
+                return GetContext.VENTA.Where(x => x.ESTATUS).Select(w => new
+                {
+                    w.ID,
+                    w.CANT_PRODUCTOS,
+                    w.IMPORTE_TOTAL
+                });
+            }
+			catch (Exception exc)
+			{
+				throw exc;
+			}
+        }
+    }
+}
---
+++ XMART-Api/Controllers/VentasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Web.Http;
+
+namespace XMART_Api.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    public class VentasController : ControllerBase
+    {
+        [System.Web.Http.HttpGet]
+        public IEnumerable<object> ObtenerListaAutoComplete()
+        {
+            try
+            {
+                var _respuesta = new BusinessLogic.Metodos.Apis().ObtenerVentasSimple();
+                return _respuesta;
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+    }
+}
